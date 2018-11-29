03cd80a9f640b5c29e194af66e85e87fd5fc9659
TaoYing <barryying>
2018-11-29 18:12:37 +0800

    1. 新增自动刷新功能 2. 新增“添加活动”和“数据统计”页面

---
+++ project/NFine.Web/Areas/MicroEvent/Controllers/AddEventController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NFine.Web.Areas.MicroEvent.Controllers
+{
+    public class AddEventController : ControllerBase
+    {
+        //
+        // GET: /MicroEvent/AddEvent/
+
+        override
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ project/NFine.Web/Areas/MicroEvent/Controllers/DatastatisticController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NFine.Web.Areas.MicroEvent.Controllers
+{
+    public class DatastatisticController : ControllerBase
+    {
+        //
+        // GET: /MicroEvent/Datastatistic/
+
+        override
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
