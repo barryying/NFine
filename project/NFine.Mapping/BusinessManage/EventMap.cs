b1feaa62bc7b3273987da2b6699e9dcd98eae5e7
TaoYing <barryying>
2018-12-04 19:38:10 +0800

    新增EventEntity逻辑和页面代码

---
+++ project/NFine.Mapping/BusinessManage/EventMap.cs
@@ -0,0 +1,14 @@
+﻿using NFine.Domain.Entity.BusinessManage;
+using System.Data.Entity.ModelConfiguration;
+
+namespace NFine.Mapping.BusinessManage
+{
+    public class EventMap : EntityTypeConfiguration<EventEntity>
+    {
+        public EventMap()
+        {
+            this.ToTable("Sys_Event");
+            this.HasKey(t => t.F_Id);
+        }
+    }
+}
--- project/NFine.Web/Areas/MicroEvent/Controllers/AddEventController.cs
+++ project/NFine.Web/Areas/MicroEvent/Controllers/AddEventController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NFine.Application.BusinessManage;
+using NFine.Code;
+using NFine.Domain.Entity.BusinessManage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +18,92 @@ namespace NFine.Web.Areas.MicroEvent.Controllers
         {
             return View();
         }
+        private EventApp eventApp = new EventApp();
 
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetTreeSelectJson()
+        {
+            var data = eventApp.GetList();
+            var treeList = new List<TreeSelectModel>();
+            foreach (EventEntity item in data)
+            {
+                TreeSelectModel treeModel = new TreeSelectModel();
+                treeModel.id = item.F_Id;
+                treeModel.text = item.F_Name;
+                treeModel.parentId = item.F_ParentId;
+                treeModel.data = item;
+                treeList.Add(treeModel);
+            }
+            return Content(treeList.TreeSelectJson());
+        }
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetTreeJson()
+        {
+            var data = eventApp.GetList();
+            var treeList = new List<TreeViewModel>();
+            foreach (EventEntity item in data)
+            {
+                TreeViewModel tree = new TreeViewModel();
+                bool hasChildren = data.Count(t => t.F_ParentId == item.F_Id) == 0 ? false : true;
+                tree.id = item.F_Id;
+                tree.text = item.F_Name;
+                tree.value = item.F_EnCode;
+                tree.parentId = item.F_ParentId;
+                tree.isexpand = true;
+                tree.complete = true;
+                tree.hasChildren = hasChildren;
+                treeList.Add(tree);
+            }
+            return Content(treeList.TreeViewJson());
+        }
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetTreeGridJson(string keyword)
+        {
+            var data = eventApp.GetList();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                data = data.TreeWhere(t => t.F_Name.Contains(keyword));
+            }
+            var treeList = new List<TreeGridModel>();
+            foreach (EventEntity item in data)
+            {
+                TreeGridModel treeModel = new TreeGridModel();
+                bool hasChildren = data.Count(t => t.F_ParentId == item.F_Id) == 0 ? false : true;
+                treeModel.id = item.F_Id;
+                treeModel.isLeaf = hasChildren;
+                treeModel.parentId = item.F_ParentId;
+                treeModel.expanded = hasChildren;
+                treeModel.entityJson = item.ToJson();
+                treeList.Add(treeModel);
+            }
+            return Content(treeList.TreeGridJson());
+        }
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetFormJson(string keyValue)
+        {
+            var data = eventApp.GetForm(keyValue);
+            return Content(data.ToJson());
+        }
+        [HttpPost]
+        [HandlerAjaxOnly]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(EventEntity eventEntity, string keyValue)
+        {
+            eventApp.SubmitForm(eventEntity, keyValue);
+            return Success("操作成功。");
+        }
+        [HttpPost]
+        [HandlerAjaxOnly]
+        [HandlerAuthorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteForm(string keyValue)
+        {
+            eventApp.DeleteForm(keyValue);
+            return Success("删除成功。");
+        }
     }
 }
